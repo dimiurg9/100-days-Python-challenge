5df6af44e2f9916c5316d43d3755bce678d23104
Dzmitry <slizhd@amazon.com>
2023-06-13 10:08:50 -0700

    list

--- Syntax/list.py
+++ Syntax/list.py
@@ -1,3 +1,11 @@
 
-fruits = [apple, orange]
+fruits = ["apple", "orange"]
+
+fruits.append("Peaches")
+fruits[0] = "Apple"
+print(fruits[1])
+print("Last item in list: " + fruits[-1])
+print("with -2: " + fruits[-2])
+print(fruits)
+print(type(fruits))
 
