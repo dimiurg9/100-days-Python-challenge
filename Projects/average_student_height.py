e2bfea1d41b0afb8052821f06c31af982cb1bcd6
Dzmitry <slizhd@amazon.com>
2023-07-31 11:08:16 -0700

    Student height added rounding

--- Projects/average_student_height.py
+++ Projects/average_student_height.py
@@ -2,6 +2,8 @@
 in dictionary of student get all student of grade 1 heights and make it average
 correct answer: 171,6
 """
+from statistics import mean
+
 Adam = {"grade":1, "heigh": 180}
 Paylik = {"grade":2, "heigh": 150}
 Vanyatko = {"grade":1, "heigh": 160}
@@ -15,7 +17,10 @@ for names in students_list:
         studens_heits.append(names["heigh"])
 
 print(round(sum(studens_heits) / len(studens_heits), 2))
-
+print(min(studens_heits))
+print(max(studens_heits))
+print("%.2f" % (mean(studens_heits)))
+print(type("%.2f" % (mean(studens_heits))))
 
 
 
