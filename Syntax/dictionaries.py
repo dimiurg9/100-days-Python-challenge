b0e45a91d6344477832eeb462641d901703b28be
Dzmitry <slizhd@amazon.com>
2023-08-31 22:56:59 -0700

    Some debugging in previous files

--- Syntax/dictionaries.py
+++ Syntax/dictionaries.py
@@ -1,27 +1,14 @@
-"""
-List of students with score to convert into grades
-91+ = outstanding
-81-90 = exceeds expectation
-71-80 = acceptable
-70 and less = fail
-"""
-students_scores = {
-    "John": 60,
-    "Bill":81,
-    "Ueban":93,
-    "Mashish":70,
-    "Rohit":90
-}
-student_grades = {}
-for key, value in students_scores.items():
-    if value >= 91:
-        student_grades[key] = "Outstanding"
-    elif value >= 81 and value <=90:
-        student_grades[key] = "Exceeds expectations"
-    elif value >= 71 and value <=80:
-        student_grades[key] = "Acceptable"
-    else:
-        student_grades[key] = "Fail"
 
-for key, value in student_grades.items():
-    print(f"{key: <20} {value}")
+my_dictionary = {
+    "a": ["aa", "bb", "cc"],
+    "b":{
+        "x":"xx",
+        "y":"yy",
+        "z":"zz"
+    }
+}
+
+print(my_dictionary["a"][1])
+print(my_dictionary["b"]["y"])
+my_dictionary["b"]["y"] = "bbbbbbb"
+print(my_dictionary["b"]["y"])
