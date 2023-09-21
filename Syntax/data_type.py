839404f24e9856096c5979a76b1a60a03ad9da25
Dzmitry <slizhd@amazon.com>
2023-09-21 11:14:11 -0700

    Added multiple variables assignment

--- Syntax/data_type.py
+++ Syntax/data_type.py
@@ -2,9 +2,15 @@ nameLength = len(input("what is you name? "))
 print(type(nameLength))
 
 # no error
-# print(f"your name have {nameLength} characters")
+print(f"your name have {nameLength} characters")
 
 #TypeError: can only concatenate str (not "int") to str
 # print("name " + nameLength + " characters")
 
-print("name " + str(nameLength)+ " characters")
+# print("name " + str(nameLength)+ " characters")
+
+a, b, c = 1, 2, "c"
+#if we don't care about some of values, we can use decoratior
+# we can use a, _, _ = return_three_values()
+
+print(f"a = {a}, b = {b}, c = {c}")
